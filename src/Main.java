6de083be13104865c169d71a74447e8b897d8bc2
PawelSzeliga <paweszeliga@gmail.com>
2023-05-02 17:14:04 +0200

    SizeSetterWindow and some new functionalities

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(MainMenuWindow::new);
-        ArrayList<Player> arrayList = new ArrayList<>();
+        /*ArrayList<Player> arrayList = new ArrayList<>();
         arrayList.add(new Player("Kasia", 2000));
-        FileStreamController.writeFile(arrayList);
+        FileStreamController.writeFile(arrayList);*/
     }
 }
