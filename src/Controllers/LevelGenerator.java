de78daed50ae2393d385ad2f4924ff5ae072b588
pawelSzeligaSzkola <paweszeliga@gmail.com>
2023-05-09 09:55:56 +0200

    LevelGenerator

--- src/Controllers/LevelGenerator.java
+++ src/Controllers/LevelGenerator.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class LevelGenerator {
@@ -9,27 +10,38 @@ public class LevelGenerator {
         int[][] level = new int[size][size];
         int x, y;
 
-        do {
-            x = (int) (Math.random() * size);
-        } while (x % 2 != 0);
+        if (size % 2 == 0) {
+            do {
+                x = (int) (Math.random() * size);
+            } while (x % 2 != 0);
 
-        do {
-            y = (int) (Math.random() * size);
-        } while (y % 2 != 0);
+            do {
+                y = (int) (Math.random() * size);
+            } while (y % 2 != 0);
+        } else {
+            do {
+                x = (int) (Math.random() * size);
+            } while (x % 2 == 0);
 
+            do {
+                y = (int) (Math.random() * size);
+            } while (y % 2 == 0);
+        }
+        System.out.println("x:" + x);
+        System.out.println("y:" + y);
         Stack<int[]> movesStack = new Stack<>();
         movesStack.push(new int[]{x, y});
 
         while (movesStack.size() > 0) {
             int[] possibleMoves = new int[4];
             int possibleMovesCounter = 0;
-            if (x - 2 >= 0 && level[y][x - 2] == 0) {
+            if (x - 2 > 0 && level[y][x - 2] == 0) {
                 possibleMoves[possibleMovesCounter++] = 0;
             }
             if (x + 2 < size && level[y][x + 2] == 0) {
                 possibleMoves[possibleMovesCounter++] = 1;
             }
-            if (y - 2 >= 0 && level[y - 2][x] == 0) {
+            if (y - 2 > 0 && level[y - 2][x] == 0) {
                 possibleMoves[possibleMovesCounter++] = 2;
             }
 
@@ -65,41 +77,45 @@ public class LevelGenerator {
                 y = lastMove[1];
             }
         }
-        int[][] finalLevel = new int[size + sizeModifier][size + sizeModifier];
-        for (int i = 0; i < finalLevel.length; i++) {
-            for (int j = 0; j < finalLevel[i].length; j++) {
-                if (i == 0 || j == 0 || i == finalLevel.length - 1 || j == finalLevel.length - 1) {
-                    finalLevel[i][j] = 0;
-                } else {
-                    finalLevel[i][j] = level[i - 1][j - 1];
+        for (int value = 1; value < 20; value += 4) {
+            for (int i = 0; i < level.length; i++) {
+                for (int j = 0; j < level[i].length; j++) {
+                    if (
+                            (i == value && j > value - 1 && j < level[i].length - value) ||
+                                    (j == value && i > value - 1 && i < level.length - value) ||
+                                    (i == level.length - (value + 1) && j > value - 1 && j < level[i].length - value) ||
+                                    (j == level[i].length - (value + 1) && i > value - 1 && i < level.length - value)
+                    ) {
+                        level[i][j] = 1;
+                    }
                 }
             }
         }
-        int[][] finalFinalLevel = new int[finalLevel.length][finalLevel.length];
-        for (int i = 0; i < finalLevel.length; i++) {
-            for (int j = 0; j < finalLevel[i].length; j++) {
-                if (finalLevel[i][j] == 0) {
+        int[][] finalLevel = new int[level.length][level.length];
+        for (int i = 0; i < level.length; i++) {
+            for (int j = 0; j < level[i].length; j++) {
+                if (level[i][j] == 0) {
                     int sum = 0;
 
-                    if (i - 1 >= 0 && finalLevel[i - 1][j] == 0) {
+                    if (i - 1 >= 0 && level[i - 1][j] == 0) {
                         sum += 1;
                     }
-                    if (i + 1 < finalLevel.length && finalLevel[i + 1][j] == 0) {
+                    if (i + 1 < level.length && level[i + 1][j] == 0) {
                         sum += 2;
                     }
-                    if (j - 1 >= 0 && finalLevel[i][j - 1] == 0) {
+                    if (j - 1 >= 0 && level[i][j - 1] == 0) {
                         sum += 4;
                     }
-                    if (j + 1 < finalLevel.length && finalLevel[i][j + 1] == 0) {
+                    if (j + 1 < level.length && level[i][j + 1] == 0) {
                         sum += 8;
                     }
-                    finalFinalLevel[i][j] = sum;
+                    finalLevel[i][j] = sum;
                 } else {
-                    finalFinalLevel[i][j] = 99;
+                    finalLevel[i][j] = 99;
                 }
             }
         }
 
-        return finalFinalLevel;
+        return finalLevel;
     }
 }
